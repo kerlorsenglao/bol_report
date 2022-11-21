2f09021594da28df427709a689e6bd0cf1657570
toumKHAM <toumdianziyoujian@gmail.com>
2022-11-21 16:00:21 +0700

    Back to AAAA

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -16,7 +16,7 @@ export default function HomeScreen({navigation}) {
             <Spinner visible={isLoading}/>
             <Text style={{color: COLORS.white, fontSize: SIZES.extraLarge}}>
                 {/* Hi, <Text style={{fontWeight:'bold'}}>{userInfo.account}</Text> */}
-                Hi, <Text style={{fontWeight:'bold'}}>AAA7</Text>
+                Hi, <Text style={{fontWeight:'bold'}}>AAAA</Text>
             </Text>
             <LottieView 
                 source={data_sharing} 
