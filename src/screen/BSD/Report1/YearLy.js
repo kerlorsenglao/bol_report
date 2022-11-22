a2b20b8d280c94b6a4b7635bc0b09f100eabb704
toumKHAM <toumdianziyoujian@gmail.com>
2022-11-22 08:45:58 +0700

    change by toum 22-11-2022 in the morning

---
+++ src/screen/BSD/Report1/YearLy.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const YearLy = () => {
+  return (
+    <View style={{flex:1,backgroundColor:"#fff"}}>
+      <Text>YearLy</Text>
+    </View>
+  )
+}
+
+export default YearLy
